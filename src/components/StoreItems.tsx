8cc58bd063aa6a8adbb7fce60161d36ccec2215f
Brooks Ryba <brooksryba@gmail.com>
2023-04-07 00:37:27 -0400

    Fetch items from api

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,52 +1,44 @@
+import { useEffect, useState } from "react";
+
+type StoreItem = {
+  id: number;
+  name: string;
+  date: number;
+  price: number;
+  image: string;
+};
+
 function StoreItems() {
+    const [items, setItems] = useState<StoreItem[]>([]);
+
+    useEffect(() => {
+      fetch("http://localhost:5000/list")
+        .then((response) => response.json())
+        .then((data: StoreItem[]) => {
+          setItems(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }, []);
+
     return (
-       <div className="StoreItems">
-        <div className="item">
-            <img alt="" src={require("images/pottery/IMG_6770.webp")}/>
-            <hr/>
-            <span className="title">Mossy Oak Mug</span><br/>
-            Price: <span className="price">$30.00</span><br/>
-            Added: <span className="date">04/05/2023</span>
-        </div>
-        <div className="item">
-            <img alt="" src={require("images/pottery/IMG_6763.webp")}/>
-            <hr/>
-            <span className="title">Cobalt Dreams Mug</span><br/>
-            Price: <span className="price">$30.00</span><br/>
-            Added: <span className="date">04/05/2023</span>
-        </div>
-        <div className="item">
-            <img alt="" src={require("images/pottery/IMG_6776.webp")}/>
-            <hr/>
-            <span className="title">Inkwell Mug</span><br/>
-            Price: <span className="price">$30.00</span><br/>
-            Added: <span className="date">04/05/2023</span>
-        </div>
-        <div className="item">
-            <img alt="" src={require("images/pottery/IMG_6797.webp")}/>
-            <hr/>
-            <span className="title">Ocean Sands Mug</span><br/>
-            Price: <span className="price">$30.00</span><br/>
-            Added: <span className="date">04/05/2023</span>
-        </div>
-        <div className="item">
-            <div className="image">
-                <img alt="" src={require("images/pottery/IMG_6799_.webp")}/>
+        <div className="StoreItems">
+            {items.map((item) => (
+            <div className="item">
+                <img alt="" src={require.context("images/pottery", false, /\.(webp)$/)(item.image)}/>
+                <hr/>
+                <span className="title">{item.name}</span><br/>
+                Price: <span className="price">${item.price}</span><br/>
+                Added: <span className="date">{new Date(item.date*1000).toLocaleDateString('en-US', {
+                    month: '2-digit',
+                    day: '2-digit',
+                    year: '2-digit',
+                    })}</span>
             </div>
-            <hr/>
-            <span className="title">Toasted Mallow Planter</span><br/>
-            Price: <span className="price">$30.00</span><br/>
-            Added: <span className="date">04/05/2023</span>
+            ))}
         </div>
-        <div className="item">
-            <img alt="" src={require("images/pottery/IMG_6792.webp")}/>
-            <hr/>
-            <span className="title">Autumn Rush Mug</span><br/>
-            Price: <span className="price">$30.00</span><br/>
-            Added: <span className="date">04/05/2023</span>
-        </div>
-        </div>
-    )
+    );
 }
 
 export default StoreItems;
